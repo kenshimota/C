8276ff0c6154019af2875f3c54baafecdb123f48
The Mot <kenshimota@gmail.com>
2017-12-19 18:29:07 -0400

    Actualize el codigo
    Vi la necesidad de validar los datos obtenidos del usuario porque si el usuario ingresaba una letra donde iba un valor numérico no podría ejecutar con total efectividad su labor el programa ademas de corregir la secuencia de scanf que estaba mal escrita como sacnf

--- Computer Oriented Statistical Methods/Simpson's_1-3rd_rule.c
+++ Computer Oriented Statistical Methods/Simpson's_1-3rd_rule.c
@@ -1,23 +1,54 @@
+/* librerias que nos permitiran el desarrollo de nuestra aplicacion  */
 #include<conio.h>
 #include<stdio.h>
-#include<math.h> 
+#include<math.h>
+
+
 float f(float x)
 {
 	return 1.0+x*x*x;
 }
-void main()
+
+/* funcion encargada de obtener un numero ingresado por el usuario 
+y retornara el valor flotante */
+
+float get_num(){
+	
+	float num = 0;
+	int count;
+	
+	//Esto hara que se repita siempre, pues mi while sera un bucle 'infinito' propiamente controlado un codicional
+	while(1){
+		/* count obtendra el valor retornado de scanf que retorna 1 si el valor ingresado por 
+		el usuario es verdadero y 0 cero si no es el valor ingresado no esta asociado a la variable */
+		count = scanf("%f",&num);
+		getchar();
+		
+		if(count == 1)
+			return num;
+		else
+			printf("El valor ingresado no es valido...\nIngrese un numero: ");
+	}
+}
+
+int main()
 {
 	int i,n;
 	float a,b,h,x,s2,s3,sum,integral;
+	
 	printf("enter the lower limit of the integration");
-	sacnf("%f",&a);
+	a = get_num();
+	
 	printf("enter the upper limit of the integration");
-	sacnf("%f",&b);
+	b = get_num();
+	
 	printf("enter the number of intervals");
-	sacnf("%d",&n);
+	n = get_num();
+	
 	h=(b-a)/n;
 	sum=f(a)+f(b);
 	s2=s3=0.0;
+	
 	for(i=1;i<n;i+=3)
 	{
 		x=a+i*h;
@@ -30,5 +61,5 @@ void main()
 	}
 	intgral=(h/3.0)*(sum+2*s2+4*s3);
 	printf("\nvalue of the integral =%9.4f\n",integral);
-	getch();
+	getchar();
 }
